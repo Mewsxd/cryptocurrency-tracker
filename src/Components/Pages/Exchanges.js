11f04a064f6da8d38f96f01879f6921acdfc1cb5
Muzammil Malik <abdmalik1593@gmail.com>
2023-08-13 16:01:02 +0530

    fixed navbar issue of disappearing when clicked and deleted App.css file

--- src/Components/Pages/Exchanges.js
+++ src/Components/Pages/Exchanges.js
@@ -1,11 +1,28 @@
 import { Avatar, Col, Row, Typography } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useGetCryptoExchangeQuery } from "../../services/CryptoExchange";
 import millify from "millify";
 import ExchangeLineChart from "../ExchangeLineChart";
 import { useSelector } from "react-redux";
 
 const Exchanges = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   const currencyCode = useSelector((state) => state.exchangeRate.currencyCode);
   const exchangeRate = useSelector((state) => state.exchangeRate.exchangeRate);
   const { Text, Title } = Typography;
@@ -24,44 +41,50 @@ const Exchanges = () => {
   return (
     <>
       <Row className="exchange-header-container">
-        <Col span={6}>
-          <Title level={3}>Exchange</Title>
+        <Col span={!activeMenu ? 10 : 6}>
+          <p className="exchange-header">Exchanges</p>
         </Col>
-        <Col span={6}>
-          <Title level={3}>24h trading volume</Title>
+        <Col span={!activeMenu ? 7 : 6}>
+          <p className="exchange-header">24h trading volume</p>
         </Col>
-        <Col span={6}>
-          <Title level={3}>Trust score</Title>
+        <Col span={!activeMenu ? 7 : 6}>
+          <p className="exchange-header">Trust score</p>
         </Col>
-        <Col span={6}>
-          <Title level={3}>Last 7 days</Title>
-        </Col>
+        {activeMenu && (
+          <Col span={6}>
+            <p className="exchange-header">Trading Volume (Last 7 days)</p>
+          </Col>
+        )}
       </Row>
       <div className="exchange-container">
         {exchangeData?.map((data) => {
           return (
             <Row className="exchange">
-              <Col span={6}>
+              <Col span={!activeMenu ? 10 : 6}>
                 <Text>
                   <strong>{data?.trust_score_rank}.</strong>
                 </Text>
                 <Avatar className="exchange-image" src={data?.image} />
-                <Text>
-                  <strong>
-                    <a href={data?.url} target="_blank" rel="noreferrer">
-                      {data?.name}
-                    </a>
-                  </strong>
-                </Text>
+                <Col>
+                  <Text>
+                    <strong>
+                      <a href={data?.url} target="_blank" rel="noreferrer">
+                        {data?.name}
+                      </a>
+                    </strong>
+                  </Text>
+                </Col>
               </Col>
-              <Col span={6}>
+              <Col span={!activeMenu ? 7 : 6}>
                 {currencySymbol}
                 {millify(data?.trade_volume_24h_btc * exchangeRate)}
               </Col>
-              <Col span={6}>{data?.trust_score}</Col>
-              <Col span={2}>
-                <ExchangeLineChart id={data?.id} />
-              </Col>
+              <Col span={!activeMenu ? 7 : 6}>{data?.trust_score}</Col>
+              {activeMenu && (
+                <Col span={2} className="line-chart">
+                  <ExchangeLineChart id={data?.id} />
+                </Col>
+              )}
             </Row>
           );
         })}
